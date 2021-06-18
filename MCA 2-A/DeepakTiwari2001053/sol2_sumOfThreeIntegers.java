cc1bd48ca8f7878424f13e9e19a4fc46ca208d29
deptiwddn <tdeepak44410@gmail.com>
2021-06-18 14:04:13 +0530

    code of ques 1-2

---
+++ MCA 2-A/DeepakTiwari2001053/sol2_sumOfThreeIntegers.java
@@ -0,0 +1,10 @@
+public sol2_sumOfThreeIntegers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5; 
+        int num3 = 20;
+        int sum = num1 + num2 +num3;
+        System.out.println(" num1 + num2 + num3 = " + sum);
+
+    }
+}
